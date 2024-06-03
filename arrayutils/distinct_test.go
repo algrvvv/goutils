c3c12bb4c48dd706eb5347ac46cbbf9880c75b8f
man0so1 <accaunt.dlya.figni@gmail.com>
2024-06-03 16:59:57 +0300

    улучшено тестирование функции Distinct()

--- arrayutils/distinct_test.go
+++ arrayutils/distinct_test.go
@@ -6,10 +6,20 @@ import (
 )
 
 func TestDistinct(t *testing.T) {
-	inputData := []int{1, 2, 3, 3, 4, 5, 5, 6, 7, 8, 8, 9, 9, 10}
-	actual := Distinct(inputData)
-	expected := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
-	if !reflect.DeepEqual(actual, expected) {
-		t.Errorf("actual %v\nexpected %v", actual, expected)
+	tests := []struct {
+		inputData []any
+		expected  []any
+	}{
+		{[]any{1, 2, 3, 3, 4, 5, 5, 6, 7, 8, 8, 9, 9, 10}, []any{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}},
+		{[]any{9, 4, 5, -4, 9, 3, 6, 4}, []any{9, 4, 5, -4, 3, 6}},
+		{[]any{"abc", "cba", "abc", "abc"}, []any{"abc", "cba"}},
+		{[]any{'a', 'b', 'c', 'c'}, []any{'a', 'b', 'c'}},
+	}
+
+	for _, test := range tests {
+		actual := Distinct(test.inputData)
+		if !reflect.DeepEqual(actual, test.expected) {
+			t.Errorf("Distinct(%+v): expected %v, actual %v", test.inputData, test.expected, actual)
+		}
 	}
 }
