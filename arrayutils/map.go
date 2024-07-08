b35c4ef5e754c36426ddfa653dda0c5a2401c32b
p4xt3r <121817930+algrvvv@users.noreply.github.com>
2024-07-08 11:11:24 +0300

    обновлен тип для функции `mapValues`

--- arrayutils/map.go
+++ arrayutils/map.go
@@ -2,7 +2,7 @@ package arrayutils
 
 // MapValues a function to iterate through an array of elements
 // and apply a callback to each element
-func MapValues[T any](values []T, mapFunc func(T) T) []T {
+func MapValues[T interface{}](values []T, mapFunc func(T) T) []T {
 	var newValues []T
 	for _, value := range values {
 		newValue := mapFunc(value)
